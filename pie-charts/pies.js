4787158f44291d293b4af9fe4de66cb5b20c443f
Joseph Misiti <joseph.misiti@gmail.com>
2012-12-24 00:58:48 -0500

    updated pie charts, which are now working

--- pie-charts/pies.js
+++ pie-charts/pies.js
@@ -3,10 +3,44 @@ SOCIALQ.models = {}
 
 SOCIALQ.models.pieChart = function() {
 	
+	
+	var innerRadius = 100;
+	var outerRadius = 200;
+	var color = d3.scale.category20();
+    var donut = d3.layout.pie().value(function(d){ return d.value });
+    var arc = d3.svg.arc().outerRadius(outerRadius);
+	
 	function chart(selection) {
     	selection.each(function(data) {
 			
-			console.log(data)
+			//data = [ 10, 20, 30, 40 ]
+			
+			var container = d3.select(this);
+			var pie_wrapper = container.selectAll('g.pie-wrapper')
+							.data([data])
+							.enter()
+							.append('g')
+							.attr('class','pie-wrapper');
+							
+			var legend_wrapper = container.selectAll('g.legend-wrapper')
+							.data([data])
+							.enter()
+							.append('g')
+							.attr('class','legend-wrapper');							
+			
+
+			var arcs = pie_wrapper.selectAll('g.arc')
+								  .data(donut)
+								  .enter().append('g')
+								  .attr('class','arc')
+								  .attr("transform", "translate(" + outerRadius + "," + outerRadius + ")");
+								
+								
+			arcs.append("path")
+			    .attr("fill", function(d, i) { return color(i); })
+			    .attr("d", arc);
+
+
 			
 		});
 	}
