8802b53d52eea928a7d511be304294c8d02a8ef0
Joseph Misiti <joseph.misiti@gmail.com>
2012-12-23 22:52:21 -0500

    added initial pie chart directory

---
+++ pie-charts/pies.js
@@ -0,0 +1,15 @@
+var SOCIALQ = {}
+SOCIALQ.models = {}
+
+SOCIALQ.models.pieChart = function() {
+	
+	function chart(selection) {
+    	selection.each(function(data) {
+			
+			console.log(data)
+			
+		});
+	}
+	
+	return chart;
+}
